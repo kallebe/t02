e9b109c2aa24c5f480e0508025cae670137a8aa4
kallebe <kallebe87@gmail.com>
2018-11-16 21:29:31 -0200

    Mudando comentario

---
+++ grafos.h
@@ -0,0 +1,24 @@
+#ifndef GRAFOS_H
+#define GRAFOS_H
+
+typedef struct vertice *Vertice;
+typedef struct grafo Grafo;
+
+struct grafo {
+    int nVertices;
+    Vertice *listaAdj;
+};
+
+//Um comentário aqui!
+
+struct vertice {
+    int v;
+    Vertice prox;
+};
+
+void criarGrafo(Grafo *G, int nVertices);
+void inserirAresta(Grafo *G, int v1, int v2);
+void imprimeGrafo(Grafo *G);
+void liberaGrafo(Grafo *G);
+
+#endif
